d7695442b737a82a2532305422ad461d54415660
Rachel Pawliuk <34349409+rpawliuk@users.noreply.github.com>
2020-08-17 21:40:08 -0600

    Create main.c
    Basic main file for the text adventure game

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   printf("Welcome Julia!\n");
+   printf("I hope you enjoy this \n");
+
+   printf("\n I hope I can make you smile :)!\n");
+   
+   return 0;
+}
