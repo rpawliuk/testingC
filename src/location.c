5b70d75525b44a38aca48a81bdb09e8bd69ee008
Rachel Pawliuk <34349409+rpawliuk@users.noreply.github.com>
2020-08-17 22:19:22 -0600

    Update location.c

--- src/location.c
+++ src/location.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <string.h>
 
+
+void movement(const char *noun, int x, int y)
+{
+  /*
+  Movement along x and y coordinates
+  */
+   int x;
+   int y;
+   
+   if(noun == "right" && x != 100){
+   /* Move right */
+     x++;
+   }
+   else if(noun == "left" && x != 0){
+   /* Move left */
+     x--;
+   }
+  else if(noun == "up" && y != 100){
+   /* Move up */
+    y++;
+   }
+  else if(noun == "down" && y != 0){
+   /* Move down */
+    y--;
+   }
+  else{
+     printf("The direction you have entered is invalid. \n");
+  }
+  
+  
+}
